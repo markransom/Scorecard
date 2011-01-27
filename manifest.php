a68584a66f123fff80eb924bb5666a224a74150b
unknown <Mark.Ransom@.UK.NWEurope.Int.KN>
2011-01-27 00:02:23 +0000

    Cache Manifest update now refreshed the page when finished

--- manifest.php
+++ manifest.php
@@ -3,6 +3,19 @@
   echo "CACHE MANIFEST\n";
 
   $hashes = "";
+  $club = "";
+
+@clearstatcache();
+$cache_time=filemtime("manifest.php");
+
+if(!empty($_GET))
+{
+  $club=$_GET["include"];
+  $hashes .= md5_file($club);
+  $file_time=filemtime($club);
+  $cache_time=($cache_time>$file_time)?$cache_time:$file_time;
+}
+
 
   $dir = new RecursiveDirectoryIterator(".");
   foreach(new RecursiveIteratorIterator($dir) as $file) {
@@ -12,9 +25,14 @@
         substr($file->getFilename(), -5) != ".html" &&
         substr($file->getFilename(), 0, 1) != ".")
     {
-      echo $file . "\n";
-      $hashes .= md5_file($file);
+        echo $file . "\n";
+        $hashes .= md5_file($file);
+		$file_time=filemtime($file);
+		$cache_time=($cache_time>$file_time)?$cache_time:$file_time;
     }
   }
-  echo "# Hash: " . md5($hashes) . "\n";
+
+  $header_text=("last-modified: " . gmdate("D, d M Y H:i:s",$cache_time) . " GMT");
+  header($header_text);
+  echo "# Hash: " . md5($hashes) . " // ".$header_text."\n";
 ?>
